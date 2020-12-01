724676c49fb647308d527a3360dfe1f72ce1b584
Vinaysheel Wagh <vinaysheel_wagh@hotmail.com>
2020-12-01 19:04:36 +0000

    Documentation and mock-data update
    Created a detailed document for PMS app and updated values of employees and departments.

--- spa-pms/src/app/mock-departments.ts
+++ spa-pms/src/app/mock-departments.ts
@@ -1,7 +1,7 @@
 import { Department } from './department';
 
 export const Departments: Department[] = [
-  { id: "1", name: 'Engineering',showDept:false },
-  { id: "2", name: 'Management',showDept:false },
-  { id: "3", name: 'Analytics', showDept:false }
+  { id: "1", name: 'Loyalty Suite',showDept:false },
+  { id: "2", name: 'Payments',showDept:false },
+  { id: "3", name: 'AI & Analytics', showDept:false }
 ];
--- spa-pms/src/app/mock-employees.ts
+++ spa-pms/src/app/mock-employees.ts
@@ -1,13 +1,85 @@
 import { Employee } from './employee';
 
 export const Employees: Employee[] = [
-  { employeeId: 1, deptId:"1", firstName: "Vinaysheel", lastName: 'Wagh', dob:"19/01/1995",speciality: "development", showEmployee: false },
-  { employeeId: 2, deptId:"1", firstName: "Amar", lastName: 'Wagh', dob:"9/11/1994",speciality: "development", showEmployee: false },
-  { employeeId: 3, deptId:"1", firstName: "Divyani", lastName: 'Walshinge', dob:"14/02/1994",speciality: "testing", showEmployee: false },
-  { employeeId: 4, deptId:"1",firstName: "Shane", lastName: 'Watson', dob:"18/03/1990",speciality: "testing", showEmployee: false },
-  { employeeId: 5, deptId:"3", firstName: "Aron", lastName: 'Finch', dob:"22/11/1989",speciality: "analytics", showEmployee: false },
-  { employeeId: 6, deptId:"3", firstName: "Sean", lastName: 'Deasy', dob:"1/1/1999",speciality: "analytics", showEmployee: false },
-  { employeeId: 7, deptId:"2", firstName: "Shivraj", lastName: 'Nashi', dob:"14/12/1985",speciality: "Project Management", showEmployee: false },
-  { employeeId: 8, deptId:"2", firstName: "Shubhashish", lastName: 'Tiwari', dob:"21/09/1984",speciality: "Account Manager", showEmployee: false },
-  { employeeId: 9, deptId:"2", firstName: "Anthony", lastName: 'Knight', dob:"13/05/1980",speciality: "Product Owner", showEmployee: false }
+  { 
+    employeeId: 1, 
+    deptId:"1", 
+    firstName: "Paul", 
+    lastName: 'Stynes', 
+    dob:"19/01/1995",
+    speciality: "Software development", 
+    showEmployee: false 
+  },
+  { 
+    employeeId: 2, 
+    deptId:"2", 
+    firstName: "Luis", 
+    lastName: 'Nardin', 
+    dob:"9/11/1994",
+    speciality: "Software Development", 
+    showEmployee: false 
+  },
+  { 
+    employeeId: 3, 
+    deptId:"1", 
+    firstName: "Sarsha", 
+    lastName: 'Rodrigues',
+     dob:"14/02/1994",
+     speciality: "Quality Assurance", 
+     showEmployee: false 
+    },
+  { 
+    employeeId: 4,
+     deptId:"3",
+     firstName: "Shane", 
+     lastName: 'Watson', 
+     dob:"18/03/1990",
+     speciality: "Quality Assurance", 
+     showEmployee: false 
+    },
+  { 
+    employeeId: 5, 
+    deptId:"3", 
+    firstName: "Aron", 
+    lastName: 'Finch', 
+    dob:"22/11/1989",
+    speciality: "Analytics", 
+    showEmployee: false 
+  },
+  { 
+    employeeId: 6, 
+    deptId:"1", 
+    firstName: "Sam", 
+    lastName: 'Sashte', 
+    dob:"1/1/1999",
+    speciality: "Analytics", 
+    showEmployee: false 
+  },
+  { 
+    employeeId: 7,
+     deptId:"1", 
+     firstName: "Shivraj", 
+     lastName: 'Nashi', 
+     dob:"14/12/1985",
+     speciality: "Project Management", 
+     showEmployee: false 
+    },
+  { 
+    employeeId: 8, 
+    deptId:"2", 
+    firstName: "Shubhashish",
+    lastName: 'Tiwari', 
+    dob:"21/09/1984",
+    speciality: "Account Management", 
+    showEmployee: false 
+  },
+  { 
+    employeeId: 9, 
+    deptId:"2", 
+    firstName: "Anthony", 
+    lastName: 'Knight', 
+    dob:"13/05/1980",
+    speciality: "Product Management", 
+    showEmployee: false 
+  }
 ];
