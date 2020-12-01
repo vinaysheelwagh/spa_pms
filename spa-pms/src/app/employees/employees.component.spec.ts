e4ef1f444daf7374d7ea4e865c17dd5e62e4a5bc
Vinaysheel Wagh <vinaysheel_wagh@hotmail.com>
2020-12-01 11:45:36 +0000

    Unit test for Employee component
    Added following unit test cases for employee component.
    1) should filter employees based on given department
    2) should toggle employee details on click
    3) should prevent default events while dragging over the element
    4) should update employees department dynamically

--- spa-pms/src/app/employees/employees.component.spec.ts
+++ spa-pms/src/app/employees/employees.component.spec.ts
@@ -1,4 +1,6 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { EventHandlerVars } from '@angular/compiler/src/compiler_util/expression_converter';
+import { ComponentFixture, TestBed, tick } from '@angular/core/testing';
+import { By, element } from 'protractor';
 
 import { EmployeesComponent } from './employees.component';
 
@@ -22,4 +24,35 @@ describe('EmployeesComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should filter employees based on given department',()=>{
+    component.departmentId = "1" 
+    component.ngOnInit()
+    component.allEmployees = component.allEmployees.filter(emp =>
+      emp.deptId === component.departmentId
+    )
+    expect(component.employees.length).toEqual(component.allEmployees.length)
+  })
+
+  it('should toggle employee details on click',()=>{
+    component.onemployeeClick(1,Event)
+    component.employees.map(emp=>{
+      if(emp.employeeId == 1){
+        expect(emp.showEmployee).toBeTruthy()
+      }
+    })
+  })
+
+  it('should prevent default events while dragging over the element',()=>{
+    const event = new DragEvent('drop'); 
+    spyOn(event, 'preventDefault');
+    component.allowDrop(event);
+    expect(event.preventDefault).toHaveBeenCalled();
+  })
+
+  it('should update employees department dynamically',()=>{
+    component.updateEmployeeDept(1,"3")
+    const employee = component.allEmployees.find(emp => emp.employeeId == 1)
+    expect(employee?.deptId).toEqual("3")
+  })
 });
